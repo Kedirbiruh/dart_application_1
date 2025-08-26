f84eaba412a3a15965bdb2af2ed4a556114f9410
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-08-26 14:13:55 +0200

    create dart with interpolation and concatention

--- bin/hello.dart
+++ bin/hello.dart
@@ -1,5 +1,20 @@
-import 'package:hello/hello.dart' as hello;
+// import 'package:hello/hello.dart' as hello;
 
 void main(List<String> arguments) {
-  print('Hello world: ${hello.calculate()}!');
+  print('Hallo!: $arguments!');
+  print("Hallo Welt!");
+  print("Hallo Jutta Jenssen!");
+
+  String hallo = "Hallo";
+  String ort = "Welt";
+  String name = "Jutta Jenssen";
+
+  print(hallo!);
+  print("Hallo $ort!");
+  print("Hallo $name!");
+
+  String begrussung = "Hallo " + ort + " und " + name + "!";
+  print(begrussung);
 }
+  
+
