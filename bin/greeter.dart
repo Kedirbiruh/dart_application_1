f03a119dca17fe59564a631eadd1302b6bcd8714
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-08-27 13:55:30 +0200

    create greeter dart

---
+++ bin/greeter.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+void main() {
+  // nachname = fragenNachname()
+  // vorname = fragenVorname()
+  // alter = fragenAlter()
+
+  // String nachname = stdin.readLineSync() ?? "";
+  // String vorname = stdin.readLineSync() ?? "";
+  // int alter = stdin.readLineSync() ?? "";
+
+
+  String nachname = leer;
+  String vorname = leer;
+  int alter = null;
+
+if (alter < 40) {
+print("Hallo $vorname!");
+}else{
+  print("Guten Morgen $nachname!");
+}
+
+  
+  print("Hallo $nachname");
+}
+
+
+
+nachname = fragenNachname() {
+
+
+}
+
+
+
+vorname = fragenVorname() {
+
+
+  }
+
+
+
+
+
+
+  alter = fragenAlter() {
+
+  }
