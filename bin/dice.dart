1471e5aa2dc82ea1ae8fc5904b8e908e25699a5b
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-08-31 19:23:04 +0200

    dice

--- bin/dice.dart
+++ bin/dice.dart
@@ -1,9 +1,50 @@
+import 'dart:io';
 import 'dart:math';
 
 void main() {
+  var random = Random();
+  List<int> wuerfe = [];
 
+  int? maxWuerfe;
+  do {
+    stdout.write('Gib die maximal Anzal der Würfe ein(Zahl > 0):');
+    maxWuerfe = int.tryParse(stdin.readLineSync() ?? '');
+    if (maxWuerfe == null || maxWuerfe <= 0) {
+      print('Ungültige Eingabe: Gib eine Zahl größer als 0.');
+    }
+  } while (maxWuerfe == null || maxWuerfe <= 0);
 
+  int vorherigerWuerf = 0;
+  int Wurfzaeler = 0;
+  int anzahlSechsen = 0;
+  bool zweiSechsen = false;
 
+  while (Wurfzaeler < maxWuerfe) {
+    int wurf = random.nextInt(6) + 1;
+    wuerfe.add(wurf);
+    Wurfzaeler++;
+    print('Wurf $Wurfzaeler: $wurf');
+
+    if (wurf == 6) {
+      anzahlSechsen++;
+    }
+
+    if (wurf == 6 && vorherigerWuerf == 6) {
+      zweiSechsen = true;
+      break;
+    }
+
+    vorherigerWuerf = wurf;
+  }
+
+  print('Alle Wuerfelergebnisse: $wuerfe');
+  print('Anzal aller Sechsen: $anzahlSechsen');
+
+  if (zweiSechsen) {
+    print('Zwei 6er hintereinander nach $Wurfzaeler würfen!');
+  } else {
+    print(
+      'Limit erreicht: $Wurfzaeler Würfe durchgeführt, ohne zwe 6er hintereinander.',
+    );
+  }
 }
-
-
